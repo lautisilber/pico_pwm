40398a70e5691ad70a32c38e06d6b43ddaf70e1d
lautisilber <lautisilbergleit@gmail.com>
2025-04-16 19:59:50 -0300

    saving progress

--- src/test_peripherals_commands.cpp
+++ src/test_peripherals_commands.cpp
@@ -1,6 +1,8 @@
 #include <pico/stdlib.h>
 #include <stdio.h>
+
 #include <StomaSense/defs.h>
+#include <StomaSense/pump.h>
 #include <StomaSense/pin_defs.h>
 #include <SmartComm.h>
 
@@ -8,7 +10,17 @@
 #include <PicoStepper.h>
 #include <PicoServo.h>
 
-void pico_stepper_delay_us(uint32_t us) {
+namespace StomaSense
+{
+    void mutex_stepper_claim_blocking() {}
+    void mutex_stepper_release() {}
+
+    void mutex_servo_claim_blocking() {}
+    void mutex_servo_release() {}
+}
+
+void pico_stepper_delay_us(uint32_t us)
+{
     sleep_us(us);
 }
 
@@ -49,7 +61,7 @@ void test_stepper()
         STEPPER_PIN_1, STEPPER_PIN_2, STEPPER_PIN_3, STEPPER_PIN_4,
         PicoStepperStepType::PICO_STEPPER_STEP_TYPE_HALF, true);
     printf("Stepper init\n");
-    
+
     pico_stepper_set_curr_pos_forced(&stepper, 0);
     printf("Stepper pos: %li\n", pico_stepper_get_curr_pos(&stepper));
     pico_stepper_move_to_pos(&stepper, 500);
@@ -61,25 +73,63 @@ void test_stepper()
     pico_stepper_release(&stepper);
 }
 
+void test_pump()
+{
+    // pico_pwm_init(&pump, PUMP_PIN, false);
+    // pico_pwm_set_freq_and_duty_u16(&pump, 100000, 0);
+    // Pump::init();
+    printf("Not implemented (errors with extern definitions of variables)\n");
+}
 
-int main() {
+void test_servo()
+{
+    printf("Servo test start\n");
+    PicoServo servo(SERVO_PIN);
+    
+    bool r = servo.attach();
+    printf("Servo attach %s\n", (r ? "true" : "false"));
+    if (!r) return;
+
+    servo.write(90);
+    sleep_ms(500);
+    servo.write(100);
+    sleep_ms(500);
+    servo.write(90);
+    sleep_ms(500);
+    servo.write(89);
+    sleep_ms(500);
+    servo.write(90);
+
+    servo.detach();
+}
+
+int main()
+{
     stdio_init_all();
     // stdio_set_chars_available_callback(char_available_cb, nullptr);
 
     for (;;)
     {
         int c = getchar_timeout_us(1000);
-        if (c < 0) continue;
+        if (c < 0)
+            continue;
 
-        switch(c) {
+        switch (c)
+        {
         case 'h':
-            printf("o");
+            printf("o, s, p, e");
             break;
         case 'o':
             printf("OK");
             break;
         case 's':
             test_stepper();
+            break;
+        case 'p':
+            test_pump();
+            break;
+        case 'e':
+            test_servo();
             break;
         default:
             // putchar(c);
